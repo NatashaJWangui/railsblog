5b166938a005b7e964bac7da4809bf5be8a5f062
Natasha Wangui <69792432+NatashaJWangui@users.noreply.github.com>
2025-06-19 12:03:16 +0300

    Added new articles form and navigation links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,26 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # Controller for new article
+  def new
+    @article = Article.new
+  end
+
+  # Create new article
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
